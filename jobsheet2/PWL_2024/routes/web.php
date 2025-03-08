81b0274ec1877bfd3b33f7801283385fe9d5038b
AlvinoValerian <alvino41127@gmail.com>
2025-03-08 15:29:55 +0700

    percobaan1

--- jobsheet2/PWL_2024/routes/web.php
+++ jobsheet2/PWL_2024/routes/web.php
@@ -44,3 +44,11 @@ Route::get('/posts/{post}/comments/{comment}',function
 Route::get('/articles/{id}', function ($id) {
     return "Halaman Artikel dengan ID $id";
 });
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama Saya '.$name;
+});
