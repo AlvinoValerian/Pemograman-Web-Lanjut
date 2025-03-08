8d1b94499e7c84c4f86ddb9aee805df7eb728624
AlvinoValerian <alvino41127@gmail.com>
2025-03-08 20:03:32 +0700

    percobaan3 akhir

--- jobsheet2/PWL_2024/routes/web.php
+++ jobsheet2/PWL_2024/routes/web.php
@@ -78,3 +78,10 @@ Route::resource('photos', PhotoController::class)->only([
 Route::resource('photos', PhotoController::class)->except([
     'create', 'store', 'update', 'destroy'
 ]);
+
+Route::get('/greeting', function (){
+    return view('blog.hello', ['name' => 'Alvino']);
+});
+
+Route::get('/greeting', [WelcomeController::class,
+'greeting']);
