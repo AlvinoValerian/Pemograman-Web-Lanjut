467f3888779425a2e8537561599e59dfe93a21e6
AlvinoValerian <alvino41127@gmail.com>
2025-03-15 04:38:48 +0700

    praktikum5

--- jobsheet2/POS/routes/web.php
+++ jobsheet2/POS/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
@@ -33,4 +34,5 @@ Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
 Route::get('/sales', [SalesController::class, '__invoke']);
 
 Route::get('/level',[LevelController::class, 'index']);
+Route::get('/kategori',[KategoriController::class, 'index']);
 
