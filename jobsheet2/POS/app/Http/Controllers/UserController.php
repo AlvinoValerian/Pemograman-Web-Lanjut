3746fb416dc06802d6449557ea990d7db4956aea
AlvinoValerian <alvino41127@gmail.com>
2025-03-15 14:41:04 +0700

    praktikum2.1

--- jobsheet2/POS/app/Http/Controllers/UserController.php
+++ jobsheet2/POS/app/Http/Controllers/UserController.php
@@ -16,21 +16,30 @@ class UserController extends Controller
 
     public function index(){
 
-        $data=[
+        // $data=[
             // 'username' => 'customer-1',
             // 'nama' => 'Pelanggan',
             // 'password' => Hash::make('12345'),
             // 'level_id' => 4
             // 'nama' => 'Pelanggan Pertama',
-            'level_id' =>5,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+            // 'level_id' =>5,
+            // 'username' => 'manager_tiga',
+            // 'nama' => 'Manager 3',
+            // 'password' => Hash::make('12345')
+            
+        // ];
+        // UserModel::create($data);
         // UserModel::where('username','customer-1')->update($data);
 
-        $user =UserModel::all();
-        return view('user',['data' => $user]);
+        // $user =UserModel::find(1);
+        //     return view('user',['data' => $user]);
+
+        $user =UserModel::findOr(28,['username', 'nama'],function(){
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+
+        // $user =UserModel::all();
+        // return view('user',['data' => $user]);
     }
 }
