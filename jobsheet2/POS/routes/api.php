2728ea295ee879aa3e743394f3673429a97651cc
AlvinoValerian <alvino41127@gmail.com>
2025-06-21 02:28:41 +0700

    praktikum3 PWL10

--- jobsheet2/POS/routes/api.php
+++ jobsheet2/POS/routes/api.php
@@ -30,3 +30,4 @@ Route::post('/login', LoginController::class)->name('login');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', LogoutController::class)->name('logout');
